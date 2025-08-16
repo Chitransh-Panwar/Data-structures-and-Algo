beda3705b92edbf3ffc652d6d3ad7df7efda4208
Chitransh-Panwar <chitranshpanwar100102@gmail.com>
2025-08-16 09:20:53 +0530

    graphs.cpp

--- graphs.cpp
+++ graphs.cpp
@@ -607,6 +607,64 @@ vector<pair<pair<int,int>,int>>  prim_algo(int n,int m ,vector<pair<pair<int,int
     
 }
 
+
+void makeset(vector<int> &parent,vector<int> &rank,int n) {
+    for(int i=0;i<n;i++) {
+        parent[i]=i;
+        rank[i]=0;
+    }
+}
+
+int findparent(vector<int> &parent,int node) {
+    if(parent[node]==node) {
+        return node;
+    }
+    return parent[node]=findparent(parent,parent[node]);
+}
+
+void unionset(int u,int v,vector<int> &parent,vector<int> &rank) {
+    u=findparent(parent,u);
+    v=findparent(parent,v);
+    if(rank[u]<rank[v]) {
+        parent[u]=v;
+    } else if(rank[v]<rank[u]) {
+        parent[v]=u;
+    } else {
+        parent[v]=u;
+        rank[u]++;
+    }
+}
+
+int disjointbyunion(vector<vector<int>> edges,int n) {
+    vector<int> parent(n);
+    vector<int> rank(n);
+    makeset(parent,rank,n);
+}
+
+bool cmp(vector<int> &a,vector<int> &b) {
+    return a[2]<b[2];
+}
+
+//krushkal algo
+int krushkal(vector<vector<int>> &edge,int n) {
+    sort(edge.begin(),edge.end(),cmp);
+    vector<int> parent(n);
+    vector<int> rank(n);
+    makeset(parent,rank,n);
+    int minweight=0;
+    for(int i=0;i<edge.size();i++) {
+        int u=findparent(parent,edge[i][0]);
+        int v=findparent(parent,edge[i][1]);
+        int wt=edge[i][2];
+
+        if(u!=v) {
+            minweight+=wt;
+            unionset(u,v,parent,rank);
+        }
+    }
+    return minweight;
+}
+
 int main() {
 
     // int n;
@@ -646,6 +704,7 @@ int main() {
     g.print_adjlist();
     return 0;
 }
+
 
 
 
